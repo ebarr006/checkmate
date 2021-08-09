56ae4a624de40d3cea6b32d1265a7ada27af8ad3
emilio <emiliobarr006@gmail.com>
2021-08-09 15:47:26 -0700

    monorepo + client + workspaces for organization

---
+++ packages/client/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+ return <h1>Checkmate has a face!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
