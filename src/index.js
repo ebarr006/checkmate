647e307fb8cbe80d23be4a9af5af82a446190f5a
emilio <emiliobarr006@gmail.com>
2021-08-09 10:58:14 -0700

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import express from 'express';
-import Prisma from '@prisma/client';
 import v1 from './routes/v1.js';
-
-const { PrismaClient } = Prisma;
-const prisma = new PrismaClient();
-
 
 const app = express();
 const port = 3000; // MOVE THIS TO ENV FILE
-
 
 app.get('/', (req, res) => {
   res.send('Server Onlineee');
